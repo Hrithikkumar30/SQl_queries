54d73eeba4a7dc0c77b804fff1e2f2f4043c1590
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2023-07-12 11:08:16 +0530

    n_th highest salary with cte

--- nth_highest_salary.sql
+++ nth_highest_salary.sql
@@ -1,1 +1,9 @@
 Select TOP 1 Salary from (Select Distinct TOP 5 Salary from Employees order by Salary Desc) Result order by Salary
+
+
+
+WITH cte AS(
+Select Salary , DENSE_RANK() over(order by Salary DESC) R_ank from Employees)
+
+Select TOP 1 Salary from cte 
+where R_ank.DENSE_RANK = 2
